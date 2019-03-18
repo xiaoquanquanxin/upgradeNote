d4b5a88e5916705a7c23488f274f864ac9edadd4
xiaoquanquanxin <xxkl123@126.com>
2019-03-18 21:56:46 +0800

    BFC,IFC,TFC,FFC

--- 题目/2019年3月11日/index.js
+++ 题目/2019年3月11日/index.js
@@ -1,38 +1,39 @@
 console.clear();
 
 let timeout = (ms) => {
-    return new Promise((res, rej)=> {
-        setTimeout(res, ms)
-    })
+	return new Promise((res, rej) => {
+		setTimeout(res, ms)
+	})
 };
 var atiem = 1111;
-let a = ()=>timeout(atiem).then(()=> {
-    console.log(1);
-    return atiem;
+let a = () => timeout(atiem).then(() => {
+	console.log(1);
+	return atiem;
 });
 var btime = 222;
-let b = ()=>timeout(btime).then(()=> {
-    console.log(2);
-    return btime;
+let b = () => timeout(btime).then(() => {
+	console.log(2);
+	return btime;
 });
 var ctime = 1111;
-let c = ()=>timeout(ctime).then(()=> {
-    console.log(3);
-    return ctime;
+let c = () => timeout(ctime).then(() => {
+	console.log(3);
+	return ctime;
 });
 
 
-let mymerge = (arr)=> {
-    return ( async function () {
-        for (var obj of arr) {
-            await  obj();
-        }
-        return arr;
-    }());
-    //return Promise.all(arr.map((t)=>t()));
+let mymerge = (arr) => {
+	return ( async function () {
+		var _arr = [];
+		for (var obj of arr) {
+			_arr.push(await  obj())
+		}
+		return _arr;
+	}());
+	//return Promise.all(arr.map((t)=>t()));
 };
 
-mymerge([a, b, c]).then((data)=> {
-    console.log('done');
-    console.log(data);
+mymerge([a, b, c]).then((data) => {
+	console.log('done');
+	console.log(data);
 });
