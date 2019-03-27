48e8768508073662cc3f2d82ccdc32b8e6f7d256
xiaoquanquanxin <xxkl123@126.com>
2019-03-27 23:55:11 +0800

    函数式编程

---
+++ test/tests/unit/index.spec.js
@@ -0,0 +1,6 @@
+describe('函数剧本的api测试', function () {
+	it('+1函数是否等于22222222222222222222222222222222', function () {
+		// expect(window.add(1)).toBe(1);
+		expect(window.add(22)).toBe(23) ;
+	})
+})
