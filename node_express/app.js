1e2b066b6114b1b48bf05cf59dce3d6b8ed6d0c2
xiaoquanquanxin <xxkl123@126.com>
2019-04-06 19:49:46 +0800

    2019年4月6日19:49:36  express

--- node_express/app.js
+++ node_express/app.js
@@ -19,7 +19,11 @@ app.get('/json', function (req, res) {
 //  render
 app.get('/render', function (req, res) {
 	//  res.render(view,[locals],callback)：渲染一个view，同时向callback传递渲染后的字符串，如果在渲染过程中有错误发生next(err)将会被自动调用。callback将会被传入一个可能发生的错误以及渲染后的页面，这样就不会自动输出了。
-
+});
+//  静态资源
+app.use(express.static('public'));
+app.get('/static', function (req, res) {
+	res.send('hello node');
 });
 const server = app.listen(8081, function () {
 	const host = server.address().address;
