1048c0b967a9673c2c15609d1269086cd5558494
xiaoquanquanxin <xxkl123@126.com>
2019-04-06 16:31:31 +0800

    2019年4月6日16:31:24 模块化

---
+++ nodejs基础/06.export_hello.js
@@ -0,0 +1,5 @@
+function hello(x) {
+	return `hello ${x}`;
+}
+
+module.exports = {hello};
---
+++ nodejs基础/06.main.js
@@ -0,0 +1,3 @@
+const hello = require('nodejs基础/06.export_hello');
+const xx = hello.hello('xxx');
+console.log(xx);
