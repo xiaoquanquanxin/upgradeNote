29a4b52f835273229f1e8687407feebce58ee048
xiaoquanquanxin <xxkl123@126.com>
2019-04-06 20:09:18 +0800

    2019年4月6日19:52:41 成功加载了express构建的项目

--- node_express/app.js
+++ node_express/app.js
@@ -23,7 +23,18 @@ app.get('/render', function (req, res) {
 //  静态资源
 app.use(express.static('public'));
 app.get('/static', function (req, res) {
-	res.sendFile( __dirname + "/views/" + "index.html" );
+	res.sendFile(__dirname + "/views/" + "index.html");
+});
+//  表单提交的位置
+//  需要body-parser包
+const bodyParser = require('body-parser');
+// 创建 application/x-www-form-urlencoded 编码解析
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+app.post('/form/action/index', urlencodedParser, function (req, res) {
+	console.log(req.body);
+	// res.send('1')
+	// res.send(`提交成功${req.body}`);
+	res.redirect(`https://www.baidu.com/s?wd=${req.body.username}&rsv_spt=1&rsv_iqid=0xcc7671160006ff70&issp=1&f=8&rsv_bp=1&rsv_idx=2&ie=utf-8&rqlang=cn&tn=baiduhome_pg&rsv_enter=1&oq=www&inputT=789&rsv_t=9f26fzzBeq073BVLGaSyrayvrn6jLk09tqgdA4YeKlrYwENStO8sRUr6TJ8TJCGfytD7&rsv_pq=f1e7bd8f00068d5f&rsv_sug2=0&rsv_sug4=1005`)
 });
 const server = app.listen(8081, function () {
 	const host = server.address().address;
