edff2e33496d547813654e9971c3018c5b6bad6a
xiaoquanquanxin <xxkl123@126.com>
2019-04-07 15:06:50 +0800

    2019年4月7日14:52:34 路由

--- node_express/app3.js
+++ node_express/app3.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = new express();
 
+(function () {
+	//  组合路由
+	const cb0 = function (req, res, next) {
+		console.log('CB0');
+		next()
+	};
+	const cb1 = function (req, res, next) {
+		console.log('CB1');
+		next()
+	};
+
+	const cb2 = function (req, res, next) {
+		console.log('CB2');
+		next();
+	};
+	app.get('/example/c', [cb0, cb1, cb2], function (req, res) {
+		res.send('啊啊啊');
+	});
+}());
+
 
 const server = app.listen(8081, function () {
 	const host = server.address().address;
