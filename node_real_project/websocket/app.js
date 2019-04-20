0eec864be6b210a4949578f47ca364a5f043b1ce
xiaoquanquanxin <xxkl123@126.com>
2019-04-20 19:26:42 +0800

    websocket实现聊天室

---
+++ node_real_project/websocket/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = new express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use('/', function (req, res) {
+	res.sendfile('index.html');
+});
+
+io.on('connection', function (socket) {
+	socket.emit('open');
+	socket.on('message', function (msg) {
+		if (!msg) {
+			return
+		}
+		console.log('服务端接收到', msg);
+		socket.emit('james', msg + '&emsp;&emsp;&emsp;' + new Date().getMinutes() + ':' + new Date().getSeconds());
+		socket.broadcast.emit('james', msg + '&emsp;&emsp;&emsp;' + new Date().getMinutes() + ':' + new Date().getSeconds())
+	});
+});
+
+http.listen(4000, function () {
+	console.log('listening on *:4000');
+});
+// app.listen(3000, function () {
+// 	console.log('listen')
+// });
