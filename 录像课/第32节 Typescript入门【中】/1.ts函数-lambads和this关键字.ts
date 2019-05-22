99b9ff504fdd86a3e46df7a369644a8e359e12c6
xiaoquanquanxin <xxkl123@126.com>
2019-05-22 18:53:52 +0800

    ts

--- 录像课/第32节 Typescript入门【中】/1.ts函数-lambads和this关键字.ts
+++ 录像课/第32节 Typescript入门【中】/1.ts函数-lambads和this关键字.ts
@@ -31,6 +31,75 @@ console.log(attr(true));
 
 
 //  类的创建
+class Person {
+    name:string;
+    age:number;
+
+    constructor(name:string, age:number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    print() {
+        return this.name + '今年' + this.age + '岁';
+    }
+}
+
+let person = new Person('quanxin', 15);
+console.log(person);
+console.log(person.print());
+
+
+//  类的继承
+class Student extends Person {
+    school:string;
+
+    constructor(name:string, age:number, school:string) {
+        super(name, age);
+        this.school = school;
+    }
+
+    goToSchool() {
+        return this.name + this.age + this.school;
+    }
+}
+let student = new Student('皮尔斯', 60, '啥学校？');
+console.log(student);
+console.log(student.goToSchool());
+
+
+//  访问修饰符
+//  public
+//  private
+class People {
+    public name:string;
+    public age:number;
+
+    constructor(name:string, age:number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    print() {
+        return this.name + this.age;
+    }
+}
+
+class Teacher extends People {
+    private  school:string;
+
+    constructor(name:string, age:number, school:string) {
+        super(name, age);
+        this.school = school;
+    }
+
+    print() {
+        return this.name + this.age + this.school;
+    }
+}
+
+let t = new Teacher('小卡', 40, '多伦多');
+console.log(t.print());
 
 
 
