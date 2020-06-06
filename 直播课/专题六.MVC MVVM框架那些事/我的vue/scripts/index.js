503b266d47f9a73deab5c384bc4645d27e708ab6
xiaoquanquanxin <xxkl123@126.com>
2020-06-06 10:20:35 +0800

    shan

--- 直播课/专题六.MVC MVVM框架那些事/我的vue/scripts/index.js
+++
@@ -1,8 +0,0 @@
-function MVVM(options){
-    const data = options.data;
-    const el = document.getElementById(options.el);
-    this.data = data;
-    observer(this, data);
-    const dom = new Compile(this,el);
-    el.appendChild(dom);
-}
